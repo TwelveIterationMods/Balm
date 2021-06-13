07601a12e32788affd4f69776cea6201e535a821
blay09 <blay09@hotmail.de>
2021-06-13 15:57:50 +0200

    DeferredObject

--- src/main/java/net/blay09/mods/forbic/block/entity/ForbicBlockEntities.java
+++ src/main/java/net/blay09/mods/forbic/block/entity/ForbicBlockEntities.java
@@ -12,6 +12,13 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class ForbicBlockEntities {
+    protected static <T extends BlockEntity> DeferredObject<BlockEntityType<T>> registerBlockEntity(ResourceLocation identifier, ForbicBlockEntityFactory<T> factory, Block... blocks) {
+        return new DeferredObject<>(() -> {
+            BlockEntityType<T> type = FabricBlockEntityTypeBuilder.create(factory, blocks).build();
+            return Registry.register(Registry.BLOCK_ENTITY_TYPE, identifier.toString(), type);
+        }).resolveImmediately();
+    }
+
     protected static <T extends BlockEntity> DeferredObject<BlockEntityType<T>> registerBlockEntity(ResourceLocation identifier, ForbicBlockEntityFactory<T> factory, DeferredObject<Block>... blocks) {
         return new DeferredObject<>(() -> {
             Block[] resolvedBlocks = Arrays.stream(blocks).map(DeferredObject::get).toArray(Block[]::new);
