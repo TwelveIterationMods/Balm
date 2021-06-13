f9666de63e01f24a9dfe3a38e520cb286431de6f
blay09 <blay09@hotmail.de>
2021-06-13 09:03:53 +0200

    Add getConfig method

--- src/main/java/net/blay09/mods/forbic/ForbicConfig.java
+++ src/main/java/net/blay09/mods/forbic/ForbicConfig.java
@@ -1,6 +1,11 @@
 package net.blay09.mods.forbic;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 
 public class ForbicConfig implements ConfigData {
+
+    public static <T extends ForbicConfig> T getConfig(Class<T> clazz) {
+        return AutoConfig.getConfigHolder(clazz).getConfig();
+    }
 }
