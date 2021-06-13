3ff2c831c228124b6aa213c234f92e2f6bb4e9d2
blay09 <blay09@hotmail.de>
2021-06-13 11:04:55 +0200

    Add ForbicTextures

---
+++ src/main/java/net/blay09/mods/forbic/client/ForbicTextures.java
@@ -0,0 +1,9 @@
+package net.blay09.mods.forbic.client;
+
+import net.minecraft.resources.ResourceLocation;
+
+public class ForbicTextures {
+    protected static void addSprite(ResourceLocation atlas, ResourceLocation location) {
+        // TODO
+    }
+}
