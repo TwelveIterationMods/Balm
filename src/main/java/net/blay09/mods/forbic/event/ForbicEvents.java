8f766e15a004cdab7b7f36feb4fd822f2a3e50b1
blay09 <blay09@hotmail.de>
2021-06-18 20:34:32 +0200

    events

--- src/main/java/net/blay09/mods/forbic/event/ForbicEvents.java
+++ src/main/java/net/blay09/mods/forbic/event/ForbicEvents.java
@@ -1,19 +1,38 @@
 package net.blay09.mods.forbic.event;
 
 
-import java.util.function.Consumer;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ForbicEvents {
-    public static void post(ForbicEvent event) {
-        // TODO
-    }
+
+    private static final List<ScreenDrawnHandler> screenDrawnHandlers = new ArrayList<>();
+    private static ScreenEvents.BeforeInit beforeInitListener = null;
+
+    public static Event<FovUpdateHandler> FOV_UPDATE = EventFactory.createArrayBacked(FovUpdateHandler.class,
+            (listeners) -> (entity) -> {
+                for (FovUpdateHandler listener : listeners) {
+                    Float fov = listener.handle(entity);
+                    if (fov != null) {
+                        return fov;
+                    }
+                }
+
+                return null;
+            });
+    ;
 
     public static void onPlayerLogin(PlayerLoginHandler handler) {
-        // TODO
+        ServerPlayConnectionEvents.JOIN.register((listener, sender, server) -> handler.handle(listener.player));
     }
 
     public static void onFovUpdate(FovUpdateHandler handler) {
-        // TODO
+        FOV_UPDATE.register(handler);
     }
 
     public static void onLivingDamage(LivingDamageHandler handler) {
@@ -25,15 +44,20 @@ public class ForbicEvents {
     }
 
     public static void onScreenInitialized(ScreenInitializedHandler handler) {
-        // TODO
+        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> handler.handle(screen));
     }
 
     public static void onScreenDrawn(ScreenDrawnHandler handler) {
-        // TODO
-    }
+        screenDrawnHandlers.add(handler);
 
-    public static <T> void addCustomListener(Class<T> clazz, Consumer<T> handler) {
-        // TODO
+        if (beforeInitListener == null) {
+            beforeInitListener = (client, screen, scaledWidth, scaledHeight) -> {
+                for (ScreenDrawnHandler drawnHandler : screenDrawnHandlers) {
+                    ScreenEvents.afterRender(screen).register((drawnScreen, matrices, mouseX, mouseY, tickDelta) -> drawnHandler.handle(drawnScreen, matrices, mouseX, mouseY));
+                }
+            };
+            ScreenEvents.BEFORE_INIT.register(beforeInitListener);
+        }
     }
 
 }
