8f2685d82d1f050d6f7b52297a1cf6aede066970
blay09 <blay09@hotmail.de>
2021-07-07 00:02:06 +0200

    Provide a default for writeScreenOpeningData so we can use ForbicMenuProvider everywhere

--- src/main/java/net/blay09/mods/forbic/menu/ForbicMenuProvider.java
+++ src/main/java/net/blay09/mods/forbic/menu/ForbicMenuProvider.java
@@ -1,6 +1,10 @@
 package net.blay09.mods.forbic.menu;
 
 import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
 
 public interface ForbicMenuProvider extends ExtendedScreenHandlerFactory {
+    @Override
+    default void writeScreenOpeningData(ServerPlayer player, FriendlyByteBuf buf) {}
 }
