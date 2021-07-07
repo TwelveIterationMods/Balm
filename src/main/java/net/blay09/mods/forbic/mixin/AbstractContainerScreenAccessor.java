12c532f259315ce243f6c012dfe5fadb5c4124b6
blay09 <blay09@hotmail.de>
2021-07-07 23:01:30 +0200

    getHoveredSlot

--- src/main/java/net/blay09/mods/forbic/mixin/AbstractContainerScreenAccessor.java
+++ src/main/java/net/blay09/mods/forbic/mixin/AbstractContainerScreenAccessor.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.forbic.mixin;
 
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
@@ -18,4 +19,7 @@ public interface AbstractContainerScreenAccessor {
 
     @Accessor
     int getImageHeight();
+
+    @Accessor
+    Slot getHoveredSlot();
 }
