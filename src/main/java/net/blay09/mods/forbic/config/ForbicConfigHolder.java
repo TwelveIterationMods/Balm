1d223fff6c7f6f3a528f6d2a5187a6175436e261
blay09 <blay09@hotmail.de>
2021-07-07 20:52:25 +0200

    ForbicConfigHolder

---
+++ src/main/java/net/blay09/mods/forbic/config/ForbicConfigHolder.java
@@ -0,0 +1,64 @@
+package net.blay09.mods.forbic.config;
+
+import net.blay09.mods.forbic.event.ForbicEvents;
+import net.blay09.mods.forbic.network.ForbicNetworking;
+import net.blay09.mods.forbic.network.SyncConfigMessage;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.player.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+
+public class ForbicConfigHolder<T extends ForbicConfig> {
+
+    private static final AtomicReference<MinecraftServer> currentServer = new AtomicReference<>();
+    private static final Map<Class<?>, ForbicConfig> activeConfigs = new HashMap<>();
+    private static final Map<Class<?>, Function<ForbicConfig, SyncConfigMessage<ForbicConfig>>> syncPacketFactories = new HashMap<>();
+
+    @SuppressWarnings("unchecked")
+    public static <T extends ForbicConfig> T getActive(Class<T> clazz) {
+        return (T) activeConfigs.get(clazz);
+    }
+
+    public static <T extends ForbicConfig> T getFallback(Class<T> clazz) {
+        return ForbicConfig.getConfig(clazz);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends ForbicConfig> SyncConfigMessage<T> getConfigSyncMessage(Class<T> clazz) {
+        Function<ForbicConfig, SyncConfigMessage<ForbicConfig>> factory = getConfigSyncMessageFactory(clazz);
+        return (SyncConfigMessage<T>) factory.apply(getFallback(clazz));
+    }
+
+    public static <T extends ForbicConfig> Function<ForbicConfig, SyncConfigMessage<ForbicConfig>> getConfigSyncMessageFactory(Class<T> clazz) {
+        return syncPacketFactories.get(clazz);
+    }
+
+    public static <T extends ForbicConfig> void setActiveConfig(Class<T> clazz, T config) {
+        activeConfigs.put(clazz, config);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends ForbicConfig> void handleSync(Player player, SyncConfigMessage<T> message) {
+        T data = message.getData();
+        setActiveConfig((Class<T>) data.getClass(), data);
+    }
+
+    public static void initialize() {
+        ForbicEvents.onServerStarted(currentServer::set);
+        ForbicEvents.onServerStopped(server -> {
+            currentServer.set(null);
+        });
+
+        ForbicEvents.onConfigReloaded(() -> {
+            if (currentServer.get() != null) {
+                for (ForbicConfig config : activeConfigs.values()) {
+                    ForbicNetworking.sendToAll(currentServer.get(), getConfigSyncMessage(config.getClass()));
+                }
+            }
+        });
+    }
+
+}
