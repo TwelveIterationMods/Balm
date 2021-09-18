e19c21ac3fc2da1520fa0f6ad75518d85518cfe7
blay09 <blay09@hotmail.de>
2021-09-18 16:12:43 +0200

    fix network handlers

--- forge/src/main/java/net/blay09/mods/balm/forge/ForgeBalm.java
+++ forge/src/main/java/net/blay09/mods/balm/forge/ForgeBalm.java
@@ -1,7 +1,14 @@
 package net.blay09.mods.balm.forge;
 
+import net.blay09.mods.balm.forge.client.ForgeBalmClient;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod("balmforge")
+@Mod("balm")
 public class ForgeBalm {
+
+    public ForgeBalm() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(ForgeBalmClient::onInitializeClient);
+    }
+
 }
--- forge/src/main/java/net/blay09/mods/balm/forge/client/ForgeBalmClient.java
+++ forge/src/main/java/net/blay09/mods/balm/forge/client/ForgeBalmClient.java
@@ -1,9 +1,10 @@
 package net.blay09.mods.balm.forge.client;
 
 import net.blay09.mods.balm.forge.network.ForgeBalmNetworking;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class ForgeBalmClient {
-    public void onInitializeClient() {
+    public static void onInitializeClient(FMLClientSetupEvent event) {
         ForgeBalmNetworking.initializeClientHandlers();
     }
 }
