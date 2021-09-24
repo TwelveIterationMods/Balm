bac1df408d006720bb1e277301ec5937b442d04b
blay09 <blay09@hotmail.de>
2021-09-24 21:47:54 +0200

    fix forge renderer init not being bound by mod container

--- forge/src/main/java/net/blay09/mods/balm/forge/client/rendering/ForgeBalmRenderers.java
+++ forge/src/main/java/net/blay09/mods/balm/forge/client/rendering/ForgeBalmRenderers.java
@@ -17,6 +17,7 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import java.util.ArrayList;
@@ -45,72 +46,80 @@ public class ForgeBalmRenderers implements BalmRenderers {
         }
     }
 
-    private final Map<ModelLayerLocation, Supplier<LayerDefinition>> layerDefinitions = new HashMap<>();
-    private final List<Pair<Supplier<BlockEntityType<?>>, BlockEntityRendererProvider<BlockEntity>>> blockEntityRenderers = new ArrayList<>();
-    private final List<ColorRegistration<BlockColor, Block>> blockColors = new ArrayList<>();
-    private final List<ColorRegistration<ItemColor, ItemLike>> itemColors = new ArrayList<>();
-    private final List<Pair<Supplier<Block>, RenderType>> blockRenderTypes = new ArrayList<>();
+    private static class Registrations {
+        public final Map<ModelLayerLocation, Supplier<LayerDefinition>> layerDefinitions = new HashMap<>();
+        public final List<Pair<Supplier<BlockEntityType<?>>, BlockEntityRendererProvider<BlockEntity>>> blockEntityRenderers = new ArrayList<>();
+        public final List<ColorRegistration<BlockColor, Block>> blockColors = new ArrayList<>();
+        public final List<ColorRegistration<ItemColor, ItemLike>> itemColors = new ArrayList<>();
+        public final List<Pair<Supplier<Block>, RenderType>> blockRenderTypes = new ArrayList<>();
+    }
+
+    private final Map<String, Registrations> registrations = new HashMap<>();
 
     @Override
     public ModelLayerLocation registerModel(ResourceLocation location, Supplier<LayerDefinition> layerDefinition) {
         ModelLayerLocation modelLayerLocation = new ModelLayerLocation(location, "main");
-        layerDefinitions.put(modelLayerLocation, layerDefinition);
+        getActiveRegistrations().layerDefinitions.put(modelLayerLocation, layerDefinition);
         return modelLayerLocation;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public <T extends BlockEntity> void registerBlockEntityRenderer(Supplier<BlockEntityType<T>> type, BlockEntityRendererProvider<? super T> provider) {
-        blockEntityRenderers.add(Pair.of(type::get, (BlockEntityRendererProvider<BlockEntity>) provider));
+        getActiveRegistrations().blockEntityRenderers.add(Pair.of(type::get, (BlockEntityRendererProvider<BlockEntity>) provider));
     }
 
     @Override
     public void registerBlockColorHandler(BlockColor color, Supplier<Block[]> blocks) {
-        blockColors.add(new ColorRegistration<>(color, blocks));
+        getActiveRegistrations().blockColors.add(new ColorRegistration<>(color, blocks));
     }
 
     @Override
     public void registerItemColorHandler(ItemColor color, Supplier<ItemLike[]> items) {
-        itemColors.add(new ColorRegistration<>(color, items));
+        getActiveRegistrations().itemColors.add(new ColorRegistration<>(color, items));
     }
 
     @Override
     public void setBlockRenderType(Supplier<Block> block, RenderType renderType) {
-        blockRenderTypes.add(Pair.of(block, renderType));
+        getActiveRegistrations().blockRenderTypes.add(Pair.of(block, renderType));
     }
 
     @SubscribeEvent
     public void setupClient(FMLClientSetupEvent event) {
-        for (Pair<Supplier<Block>, RenderType> entry : blockRenderTypes) {
+        for (Pair<Supplier<Block>, RenderType> entry : getActiveRegistrations().blockRenderTypes) {
             ItemBlockRenderTypes.setRenderLayer(entry.getFirst().get(), entry.getSecond());
         }
     }
 
     @SubscribeEvent
     public void initRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        for (Pair<Supplier<BlockEntityType<?>>, BlockEntityRendererProvider<BlockEntity>> entry : blockEntityRenderers) {
+        for (Pair<Supplier<BlockEntityType<?>>, BlockEntityRendererProvider<BlockEntity>> entry : getActiveRegistrations().blockEntityRenderers) {
             event.registerBlockEntityRenderer(entry.getFirst().get(), entry.getSecond());
         }
     }
 
     @SubscribeEvent
     public void initLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
-        for (Map.Entry<ModelLayerLocation, Supplier<LayerDefinition>> entry : layerDefinitions.entrySet()) {
+        for (Map.Entry<ModelLayerLocation, Supplier<LayerDefinition>> entry : getActiveRegistrations().layerDefinitions.entrySet()) {
             event.registerLayerDefinition(entry.getKey(), entry.getValue());
         }
     }
 
     @SubscribeEvent
     public void initBlockColors(ColorHandlerEvent.Block event) {
-        for (ColorRegistration<BlockColor, Block> blockColor : blockColors) {
+        for (ColorRegistration<BlockColor, Block> blockColor : getActiveRegistrations().blockColors) {
             event.getBlockColors().register(blockColor.getColor(), blockColor.getObjects().get());
         }
     }
 
     @SubscribeEvent
     public void initItemColors(ColorHandlerEvent.Item event) {
-        for (ColorRegistration<ItemColor, ItemLike> itemColor : itemColors) {
+        for (ColorRegistration<ItemColor, ItemLike> itemColor : getActiveRegistrations().itemColors) {
             event.getItemColors().register(itemColor.getColor(), itemColor.getObjects().get());
         }
+    }
+
+    private Registrations getActiveRegistrations() {
+        return registrations.computeIfAbsent(ModLoadingContext.get().getActiveNamespace(), it -> new Registrations());
     }
 }
