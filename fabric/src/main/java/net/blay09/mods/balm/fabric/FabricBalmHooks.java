aca2b44a047237efba0fee4b70847cecef64a8ed
blay09 <blay09@hotmail.de>
2021-10-12 20:24:53 +0200

    empty impl

--- fabric/src/main/java/net/blay09/mods/balm/fabric/FabricBalmHooks.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/FabricBalmHooks.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.Container;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BoneMealItem;
@@ -14,6 +15,8 @@ import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
@@ -69,5 +72,11 @@ public class FabricBalmHooks implements BalmHooks {
     @Override
     public void firePlayerCraftingEvent(Player player, ItemStack crafted, Container craftMatrix) {
         // TODO
+    }
+
+    @Override
+    public boolean useFluidTank(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        // TODO
+        return false;
     }
 }
