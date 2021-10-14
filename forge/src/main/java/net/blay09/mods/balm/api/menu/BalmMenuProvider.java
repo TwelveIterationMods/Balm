3a3d633a3cb253d13486fdd735f2c455f02852a9
blay09 <blay09@hotmail.de>
2021-10-14 20:24:47 +0200

    Auto write pos to menu providers directly on block entities

--- forge/src/main/java/net/blay09/mods/balm/api/menu/BalmMenuProvider.java
+++ forge/src/main/java/net/blay09/mods/balm/api/menu/BalmMenuProvider.java
@@ -3,8 +3,13 @@ package net.blay09.mods.balm.api.menu;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
 public interface BalmMenuProvider extends MenuProvider, BalmMenuProviderContract {
     @Override
-    default void writeScreenOpeningData(ServerPlayer player, FriendlyByteBuf buf) {}
+    default void writeScreenOpeningData(ServerPlayer player, FriendlyByteBuf buf) {
+        if (this instanceof BlockEntity blockEntity) {
+            buf.writeBlockPos(blockEntity.getBlockPos());
+        }
+    }
 }
