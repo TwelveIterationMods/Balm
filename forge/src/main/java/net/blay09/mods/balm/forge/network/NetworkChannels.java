5b549092a6ce01d15cd3a9ef0bd2bf475b44f9fc
blay09 <blay09@hotmail.de>
2021-10-14 20:16:36 +0200

    Fix CME in Forge NetworkChannels

--- forge/src/main/java/net/blay09/mods/balm/forge/network/NetworkChannels.java
+++ forge/src/main/java/net/blay09/mods/balm/forge/network/NetworkChannels.java
@@ -6,10 +6,11 @@ import net.minecraftforge.fmllegacy.network.simple.SimpleChannel;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class NetworkChannels {
     private static final String version = "1.0"; // all mods will just have same version for now until I clean this up
-    private static final Map<String, SimpleChannel> channels = new HashMap<>();
+    private static final Map<String, SimpleChannel> channels = new ConcurrentHashMap<>();
 
     public static SimpleChannel get(String modId) {
         return channels.computeIfAbsent(modId, key -> NetworkRegistry.newSimpleChannel(new ResourceLocation(key, "network"), () -> version, it -> it.equals(version), it -> it.equals(version)));
