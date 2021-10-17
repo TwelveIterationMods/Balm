b65194ebe97cf193bd5af199725f3af1e3e406d3
blay09 <blay09@hotmail.de>
2021-10-17 20:09:12 +0200

    Just make all balm events cancellable in Forge

--- forge/src/main/java/net/blay09/mods/balm/api/event/BalmEvent.java
+++ forge/src/main/java/net/blay09/mods/balm/api/event/BalmEvent.java
@@ -1,6 +1,8 @@
 package net.blay09.mods.balm.api.event;
 
+import net.minecraftforge.eventbus.api.Cancelable;
 import net.minecraftforge.eventbus.api.Event;
 
+@Cancelable
 public class BalmEvent extends Event implements BalmEventContract {
 }
