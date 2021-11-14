2ce94ff02664e1ef127359ee8886b041d5891786
blay09 <blay09@hotmail.de>
2021-11-14 16:10:14 +0100

    Remove unused stuff from TossItemEvent to make it easier for Fabric

--- shared/java/net/blay09/mods/balm/api/event/TossItemEvent.java
+++ shared/java/net/blay09/mods/balm/api/event/TossItemEvent.java
@@ -1,22 +1,15 @@
 package net.blay09.mods.balm.api.event;
 
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 
 public class TossItemEvent extends BalmEvent {
     private final Player player;
-    private final ItemEntity itemEntity;
 
-    public TossItemEvent(Player player, ItemEntity itemEntity) {
+    public TossItemEvent(Player player) {
         this.player = player;
-        this.itemEntity = itemEntity;
     }
 
     public Player getPlayer() {
         return player;
-    }
-
-    public ItemEntity getItemEntity() {
-        return itemEntity;
     }
 }
