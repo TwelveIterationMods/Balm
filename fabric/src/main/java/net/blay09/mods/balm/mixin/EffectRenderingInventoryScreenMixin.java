332d9350c797be0edaef22646158601b04ae1c5a
blay09 <blay09@hotmail.de>
2021-11-30 21:45:25 +0100

    Fix wrong java version in pipeline

--- fabric/src/main/java/net/blay09/mods/balm/mixin/EffectRenderingInventoryScreenMixin.java
+++ fabric/src/main/java/net/blay09/mods/balm/mixin/EffectRenderingInventoryScreenMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EffectRenderingInventoryScreen.class)
+@Deprecated
 public abstract class EffectRenderingInventoryScreenMixin<T extends AbstractContainerMenu> extends AbstractContainerScreen<T> {
 
     public EffectRenderingInventoryScreenMixin(T abstractContainerMenu, Inventory inventory, Component component) {
