4a8335792a7050173f9b0e944f5e65572b813ac3
blay09 <blay09@hotmail.de>
2022-02-21 22:03:15 +0100

    Fix block entity sync on Fabric

--- fabric/src/main/java/net/blay09/mods/balm/api/block/entity/BalmBlockEntity.java
+++ fabric/src/main/java/net/blay09/mods/balm/api/block/entity/BalmBlockEntity.java
@@ -2,6 +2,7 @@ package net.blay09.mods.balm.api.block.entity;
 
 import net.blay09.mods.balm.api.block.BalmBlockEntityContract;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -12,6 +13,11 @@ import org.jetbrains.annotations.Nullable;
 public class BalmBlockEntity extends BlockEntity implements BalmBlockEntityContract {
     public BalmBlockEntity(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
         super(blockEntityType, blockPos, blockState);
+    }
+
+    @Override
+    public CompoundTag getUpdateTag() {
+        return createUpdateTag();
     }
 
     @Override
