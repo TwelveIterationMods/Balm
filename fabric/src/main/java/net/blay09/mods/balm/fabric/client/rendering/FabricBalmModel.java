a9d4e515cad2b2c4832f17cc6dc9f2d56aa3b1f9
blay09 <blay09@hotmail.de>
2022-06-17 21:09:51 +0200

    dirt

---
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/client/rendering/FabricBalmModel.java
@@ -0,0 +1,6 @@
+package net.blay09.mods.balm.fabric.client.rendering;
+
+import net.minecraft.client.resources.model.UnbakedModel;
+
+//public class FabricBalmModel implements UnbakedModel {
+//}
--- fabric/src/main/java/net/blay09/mods/balm/fabric/client/rendering/FabricBalmModels.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/client/rendering/FabricBalmModels.java
@@ -3,10 +3,16 @@ package net.blay09.mods.balm.fabric.client.rendering;
 import com.mojang.math.Matrix4f;
 import net.blay09.mods.balm.api.DeferredObject;
 import net.blay09.mods.balm.api.client.rendering.BalmModels;
+import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.Model;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.ModelManager;
 import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
@@ -18,26 +24,25 @@ import java.util.function.Supplier;
 public class FabricBalmModels implements BalmModels { // TODO Models
     @Override
     public DeferredObject<BakedModel> loadModel(ResourceLocation identifier) {
-        return null;
+        return DeferredObject.of(identifier, Minecraft.getInstance().getBlockRenderer().getBlockModel(Blocks.DIRT.defaultBlockState()));
     }
 
     @Override
     public DeferredObject<BakedModel> bakeModel(ResourceLocation identifier, UnbakedModel model) {
-        return null;
+        return DeferredObject.of(identifier, Minecraft.getInstance().getBlockRenderer().getBlockModel(Blocks.DIRT.defaultBlockState()));
     }
 
     @Override
     public DeferredObject<BakedModel> loadDynamicModel(ResourceLocation identifier, @Nullable Function<BlockState, ResourceLocation> modelFunction, @Nullable Function<BlockState, Map<String, String>> textureMapFunction, @Nullable BiConsumer<BlockState, Matrix4f> transformFunction) {
-        return null;
+        return DeferredObject.of(identifier, Minecraft.getInstance().getBlockRenderer().getBlockModel(Blocks.DIRT.defaultBlockState()));
     }
 
     @Override
     public DeferredObject<BakedModel> retexture(ResourceLocation identifier, Map<String, String> textureMap) {
-        return null;
+        return DeferredObject.of(identifier, Minecraft.getInstance().getBlockRenderer().getBlockModel(Blocks.DIRT.defaultBlockState()));
     }
 
     @Override
     public void overrideModel(Supplier<Block> block, Supplier<BakedModel> model) {
-
     }
 }
