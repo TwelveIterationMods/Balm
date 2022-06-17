70a2d7c56c07c920e7637d595b3731aee63e61a4
blay09 <blay09@hotmail.de>
2022-06-17 22:30:48 +0200

    WIP models

--- fabric/src/main/java/net/blay09/mods/balm/fabric/client/rendering/FabricBalmModels.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/client/rendering/FabricBalmModels.java
@@ -1,19 +1,14 @@
 package net.blay09.mods.balm.fabric.client.rendering;
 
-import com.mojang.datafixers.util.Either;
 import com.mojang.datafixers.util.Pair;
 import com.mojang.math.Matrix4f;
+import com.mojang.math.Transformation;
 import net.blay09.mods.balm.api.DeferredObject;
 import net.blay09.mods.balm.api.client.rendering.BalmModels;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.BlockModelShaper;
-import net.minecraft.client.renderer.block.model.BlockModel;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
-import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.resources.model.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
@@ -36,10 +31,13 @@ public class FabricBalmModels implements BalmModels {
         public abstract BakedModel resolve(ModelBakery modelBakery);
     }
 
+    private static ModelBakery bakery;
     private List<DeferredModel> modelsToBake = new ArrayList<>();
     public final List<Pair<Supplier<Block>, Supplier<BakedModel>>> overrides = new ArrayList<>();
 
     public void onModelBake(ModelBakery bakery) {
+        this.bakery = bakery;
+
         for (DeferredModel model : modelsToBake) {
             model.resolveAndSet(bakery);
         }
@@ -111,17 +109,18 @@ public class FabricBalmModels implements BalmModels {
         overrides.add(Pair.of(block, model));
     }
 
-    public static UnbakedModel retexture(ModelBakery bakery, ResourceLocation identifier, Map<String, String> textureMap) {
-        Map<String, Either<Material, String>> replacedTexturesMapped = new HashMap<>();
-        for (Map.Entry<String, String> entry : textureMap.entrySet()) {
-            replacedTexturesMapped.put(entry.getKey(), Either.left(new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(entry.getValue()))));
-        }
+    @Override
+    public ModelState getModelState(Transformation transformation) {
+        return BlockModelRotation.X0_Y0; // TODO Models
+    }
 
-        BlockModel blockModel = new BlockModel(identifier, Collections.emptyList(), replacedTexturesMapped, false, BlockModel.GuiLight.FRONT, ItemTransforms.NO_TRANSFORMS, Collections.emptyList());
+    @Override
+    public UnbakedModel getUnbakedModelOrMissing(ResourceLocation location) {
+        return bakery.getModel(location);
+    }
 
-        // We have to call getMaterials to initialize the parent field in the model (as that is usually done during stitching, which we're already past)
-        blockModel.getMaterials(bakery::getModel, new HashSet<>());
-
-        return blockModel;
+    @Override
+    public UnbakedModel getUnbakedMissingModel() {
+        return bakery.getModel(ModelBakery.MISSING_MODEL_LOCATION);
     }
 }
