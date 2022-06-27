3f4862f0bed0c79dac2749ef1cc2651b12946471
blay09 <blay09@hotmail.de>
2022-06-27 18:14:49 +0200

    Register container and fluid tank providers

--- fabric/src/main/java/net/blay09/mods/balm/fabric/FabricBalm.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/FabricBalm.java
@@ -3,9 +3,13 @@ package net.blay09.mods.balm.fabric;
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.config.AbstractBalmConfig;
 import net.blay09.mods.balm.api.entity.BalmEntity;
+import net.blay09.mods.balm.api.fluid.FluidTank;
+import net.blay09.mods.balm.fabric.provider.FabricBalmProviders;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.Container;
 
 public class FabricBalm implements ModInitializer {
 
@@ -18,5 +22,9 @@ public class FabricBalm implements ModInitializer {
             CompoundTag data = ((BalmEntity) oldPlayer).getBalmData();
             ((BalmEntity) newPlayer).setBalmData(data);
         });
+
+        var providers = ((FabricBalmProviders) Balm.getProviders());
+        providers.registerProvider(new ResourceLocation("balm", "container"), Container.class);
+        providers.registerProvider(new ResourceLocation("balm", "fluid_tank"), FluidTank.class);
     }
 }
