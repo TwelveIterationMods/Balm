d4684f8bf33294acd4e324339116f9f683558b00
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-01-23 14:32:03 +0100

    fix: Fix OpenScreenEvent on Forge always overriding newScreen even if other mods tried to change it https://github.com/TwelveIterationMods/HardcoreRevival/issues/93

--- shared/java/net/blay09/mods/balm/api/event/client/OpenScreenEvent.java
+++ shared/java/net/blay09/mods/balm/api/event/client/OpenScreenEvent.java
@@ -2,9 +2,11 @@ package net.blay09.mods.balm.api.event.client;
 
 import net.blay09.mods.balm.api.event.BalmEvent;
 import net.minecraft.client.gui.screens.Screen;
+import org.jetbrains.annotations.Nullable;
 
 public class OpenScreenEvent extends BalmEvent {
     private Screen screen;
+    private Screen newScreen;
 
     public OpenScreenEvent(Screen screen) {
         this.screen = screen;
@@ -14,7 +16,12 @@ public class OpenScreenEvent extends BalmEvent {
         return screen;
     }
 
+    @Nullable
+    public Screen getNewScreen() {
+        return newScreen;
+    }
+
     public void setScreen(Screen screen) {
-        this.screen = screen;
+        this.newScreen = screen;
     }
 }
