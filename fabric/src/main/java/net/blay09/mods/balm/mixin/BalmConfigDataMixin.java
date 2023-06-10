29a961863527094b992d688f3ca020878864576e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-06-10 23:12:42 +0200

    fix: Remove obsolete BalmConfigData mixin

--- fabric/src/main/java/net/blay09/mods/balm/mixin/BalmConfigDataMixin.java
+++
@@ -1,9 +0,0 @@
-package net.blay09.mods.balm.mixin;
-
-import me.shedaniel.autoconfig.ConfigData;
-import net.blay09.mods.balm.api.config.BalmConfigData;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(BalmConfigData.class)
-public interface BalmConfigDataMixin extends ConfigData {
-}
