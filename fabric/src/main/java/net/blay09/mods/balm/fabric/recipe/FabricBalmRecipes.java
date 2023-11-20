62925aa0fb7b40f71a78518e54f616dc48f0cb08
edralzar <edralzar@gmail.com>
2023-11-20 12:20:01 +0100

    feat: Take serializer when registering a recipe

--- fabric/src/main/java/net/blay09/mods/balm/fabric/recipe/FabricBalmRecipes.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/recipe/FabricBalmRecipes.java
@@ -6,6 +6,7 @@ import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 
 import java.util.function.Supplier;
@@ -13,9 +14,10 @@ import java.util.function.Supplier;
 public class FabricBalmRecipes implements BalmRecipes {
 
     @Override
-    public <T extends Recipe<?>> DeferredObject<RecipeType<T>> registerRecipeType(Supplier<RecipeType<T>> supplier, ResourceLocation identifier) {
+    public <T extends Recipe<?>> DeferredObject<RecipeType<T>> registerRecipeType(Supplier<RecipeType<T>> typeSupplier, Supplier<RecipeSerializer<T>> serializerSupplier, ResourceLocation identifier) {
         return new DeferredObject<>(identifier, () -> {
-            RecipeType<T> recipeType = supplier.get();
+            Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, identifier, serializerSupplier.get());
+            RecipeType<T> recipeType = typeSupplier.get();
             recipeType = Registry.register(BuiltInRegistries.RECIPE_TYPE, identifier, recipeType);
             return recipeType;
         }).resolveImmediately();
--- forge/src/main/java/net/blay09/mods/balm/forge/recipe/ForgeBalmRecipes.java
+++ forge/src/main/java/net/blay09/mods/balm/forge/recipe/ForgeBalmRecipes.java
@@ -5,6 +5,7 @@ import net.blay09.mods.balm.api.recipe.BalmRecipes;
 import net.blay09.mods.balm.forge.DeferredRegisters;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,10 +15,14 @@ import java.util.function.Supplier;
 
 public class ForgeBalmRecipes implements BalmRecipes {
     @Override
-    public <T extends Recipe<?>> DeferredObject<RecipeType<T>> registerRecipeType(Supplier<RecipeType<T>> supplier, ResourceLocation identifier) {
-        DeferredRegister<RecipeType<?>> register = DeferredRegisters.get(ForgeRegistries.RECIPE_TYPES, identifier.getNamespace());
-        RegistryObject<RecipeType<T>> registryObject = register.register(identifier.getPath(), supplier);
-        return new DeferredObject<>(identifier, registryObject, registryObject::isPresent);
+    public <T extends Recipe<?>> DeferredObject<RecipeType<T>> registerRecipeType(Supplier<RecipeType<T>> typeSupplier, Supplier<RecipeSerializer<T>> serializerSupplier, ResourceLocation identifier) {
+        DeferredRegister<RecipeSerializer<?>> serializerRegister = DeferredRegisters.get(ForgeRegistries.RECIPE_SERIALIZERS, identifier.getNamespace());
+        RegistryObject<RecipeSerializer<T>> serializerObject = serializerRegister.register(identifier.getPath(), serializerSupplier);
+
+        DeferredRegister<RecipeType<?>> typeRegister = DeferredRegisters.get(ForgeRegistries.RECIPE_TYPES, identifier.getNamespace());
+        RegistryObject<RecipeType<T>> typeObject = typeRegister.register(identifier.getPath(), typeSupplier);
+
+        return new DeferredObject<>(identifier, typeObject, () -> typeObject.isPresent() && serializerObject.isPresent());
     }
 
 }
