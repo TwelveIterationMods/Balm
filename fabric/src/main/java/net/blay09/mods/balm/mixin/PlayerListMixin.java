5f48fe8b25042fef32091a205dcc2f50da9e5b07
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-12-07 14:59:29 +0100

    feat: Add PlayerConnectedEvent that fires earlier than Forge's PlayerLogin, move Fabric's PlayerLogin to fire same time as Forge's PlayerLogin for consistency

---
+++ fabric/src/main/java/net/blay09/mods/balm/mixin/PlayerListMixin.java
@@ -0,0 +1,21 @@
+package net.blay09.mods.balm.mixin;
+
+import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.event.PlayerLoginEvent;
+import net.minecraft.network.Connection;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.CommonListenerCookie;
+import net.minecraft.server.players.PlayerList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerList.class)
+public class PlayerListMixin {
+    @Inject(method = "placeNewPlayer", at = @At(value = "RETURN"))
+    private void handlePlayerConnection(Connection connection, ServerPlayer player, CommonListenerCookie commonListenerCookie, CallbackInfo callbackInfo) {
+        final PlayerLoginEvent event = new PlayerLoginEvent(player);
+        Balm.getEvents().fireEvent(event);
+    }
+}
---
+++ forge/src/main/java/net/blay09/mods/balm/mixin/PlayerListMixin.java
@@ -0,0 +1,21 @@
+package net.blay09.mods.balm.mixin;
+
+import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.event.PlayerConnectedEvent;
+import net.minecraft.network.Connection;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.CommonListenerCookie;
+import net.minecraft.server.players.PlayerList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerList.class)
+public class PlayerListMixin {
+    @Inject(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/game/ClientboundPlayerAbilitiesPacket;<init>(Lnet/minecraft/world/entity/player/Abilities;)V"))
+    private void handlePlayerConnection(Connection connection, ServerPlayer player, CommonListenerCookie commonListenerCookie, CallbackInfo callbackInfo) {
+        final PlayerConnectedEvent event = new PlayerConnectedEvent(player);
+        Balm.getEvents().fireEvent(event);
+    }
+}
