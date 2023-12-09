74677c98bc5ba7f2d44d244d12567d5084afef97
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-12-09 17:00:22 +0100

    fix: Fix cherry pick compile error

--- forge/src/main/java/net/blay09/mods/balm/mixin/PlayerListMixin.java
+++ forge/src/main/java/net/blay09/mods/balm/mixin/PlayerListMixin.java
@@ -4,7 +4,6 @@ import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.event.PlayerConnectedEvent;
 import net.minecraft.network.Connection;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerList.class)
 public class PlayerListMixin {
     @Inject(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/game/ClientboundPlayerAbilitiesPacket;<init>(Lnet/minecraft/world/entity/player/Abilities;)V"))
-    private void handlePlayerConnection(Connection connection, ServerPlayer player, CommonListenerCookie commonListenerCookie, CallbackInfo callbackInfo) {
+    private void handlePlayerConnection(Connection connection, ServerPlayer player, CallbackInfo callbackInfo) {
         final PlayerConnectedEvent event = new PlayerConnectedEvent(player);
         Balm.getEvents().fireEvent(event);
     }
