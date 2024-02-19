539a798db74c8c9a3dfc041750098a1623aef5e3
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-02-19 20:34:57 +0100

    fix: Use ModelLoadingPlugin to fix additional model loading in Fabric

--- fabric/src/main/java/net/blay09/mods/balm/fabric/client/FabricBalmClient.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/client/FabricBalmClient.java
@@ -1,10 +1,13 @@
 package net.blay09.mods.balm.fabric.client;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.client.BalmClient;
 import net.blay09.mods.balm.api.event.client.DisconnectedFromServerEvent;
+import net.blay09.mods.balm.fabric.client.rendering.FabricBalmModels;
 import net.blay09.mods.balm.fabric.network.FabricBalmNetworking;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 
 public class FabricBalmClient implements ClientModInitializer {
     @Override
@@ -12,5 +15,7 @@ public class FabricBalmClient implements ClientModInitializer {
         ClientLifecycleEvents.CLIENT_STARTED.register(client -> FabricBalmNetworking.initializeClientHandlers());
 
         Balm.getEvents().onEvent(DisconnectedFromServerEvent.class, event -> Balm.getConfig().resetToBackingConfigs());
+
+        ModelLoadingPlugin.register((FabricBalmModels) BalmClient.getModels());
     }
 }
