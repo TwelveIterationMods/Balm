a9057f60e500d7a19a719ea91c99dea370e00237
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-08 11:02:10 +0200

    refactor(26.3): Disable Loot Modifiers temporarily

--- fabric/src/main/java/net/blay09/mods/balm/fabric/internal/FabricBalmLootModifiers.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/internal/FabricBalmLootModifiers.java
@@ -2,7 +2,6 @@ package net.blay09.mods.balm.fabric.internal;
 
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.world.level.storage.loot.internal.CommonBalmLootTables;
-import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.core.Holder;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
@@ -16,7 +15,7 @@ public final class FabricBalmLootModifiers {
     }
 
     public static void initialize() {
-        LootTableEvents.MODIFY_DROPS.register((holder, lootContext, drops) -> applyModifiers(holder, drops, lootContext));
+        // TODO LootTableEvents.MODIFY_DROPS.register((holder, lootContext, drops) -> applyModifiers(holder, drops, lootContext));
     }
 
     private static void applyModifiers(Holder<LootTable> holder, List<ItemStack> drops, LootContext lootContext) {
